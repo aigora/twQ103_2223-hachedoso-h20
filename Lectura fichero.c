8ecd5588ba50af2740e3c13b8f3a8d98e713e14c
jjuezz <j.juez@alumnos.upm.es>
2023-05-19 19:26:01 +0200

    Update Lectura fichero.c

--- Lectura fichero.c
+++ Lectura fichero.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
-#define N 200
+#include <string.h>
+#define N 250
+
+
+typedef struct {
+	
+} parametros;
+
+
+void vacio();
+void buscar();
+void comparar();
+void ordenar();
+void estadistica();
+void salir();
 
 
 int main (){
 	char aux[N];
+	int op;
+	char key;
 	
 	FILE *f;
 	
@@ -14,12 +30,60 @@ int main (){
 		exit(1);
 	}
 	
-	while (!feof(f)) {
+	/*while (!feof(f)) {
 		fgets(aux, N, f);
 		printf("%s", aux);
-	}
+	}*/
 	
 	fclose(f);
 	
+	
+	printf("                      //////////////////////////////////////////////////////////////////////////////\n");
+	printf("                      //////////////////////////////////MENU DE INICIO//////////////////////////////\n");
+	printf("                      /////////////////////////////////////////////////////////////////////////////\n\n\n");
+	
+	do {
+		do {
+			printf("Elige una opcion:\n");
+			printf("(1) Buscar datos:\n");
+			printf("(2) Comparar datos:\n");
+			printf("(3) Ordenar datos:\n");
+			printf("(4) Estadisticas:\n");
+			scanf("%d", &op);
+		} while (op < 1 || op > 4);
+		
+		switch(op) {
+			case 1: buscar() ; break;
+			case 2: comparar() ; break;
+			case 3: ordenar() ; break;
+			case 4: estadistica() ; break;
+		}
+		
+		printf("Desea realizar otra operacion (S/N)\n");
+		fflush(stdin);
+		scanf("%c", &key);
+		
+	} while (key == 's' || key == 'S');
+	
 	return 0;
+}
+
+
+void buscar() {
+	
+}
+
+
+void comparar() {
+	
+}
+
+
+void ordenar() {
+	
+}
+
+
+void estadistica() {
+	
 }
